2ebd49742d1819960f21b5fd483ab55e33f78bd6
Loretta <loretta.lee@flextronics.com>
2013-12-01 16:33:42 -0500

    initial respository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OneApp::Application.config.secret_key_base = 'cae735cfd401c71b66fd37e6c1580bede05d140ee7cca2bcdcfe56eb097795f9d6c6e5cbb4911f3a260bd76776f7e95f35b88a33e2aaf34e003502fe465ab2a7'
